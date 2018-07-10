5703a280e148ae2e360c8865a0ccc55f7ed620d7
ellen.l.hendricks@gmail.com <ellen.hendricks@sprydigital.com>
2018-07-09 23:02:21 -0500

    Initial Commit

---
+++ PyPoll/main.py
@@ -0,0 +1,50 @@
+# PyPoll application
+
+# Last row incomplete
+# data: 19129737, Queen, no candidate
+
+import csv
+import os
+
+# Path to input file
+pybank_csv = os.path.join("..", "..", "election_data.csv")
+
+with open(pybank_csv, newline="") as csvfile:
+    cin = csv.reader(csvfile, delimiter=",")
+    next(cin) # Skip header
+
+    # Initialization
+    votes = 0
+    candidates = []
+
+    # Calculations
+    for row in cin:
+        # Compute total votes
+        votes += 1
+
+        # Find candidates
+        if (votes >= 2):
+            if row[2] not in candidates:
+                # Add new candidate
+                candidates.append(row[2])
+        else:
+            # First candidate
+            candidates.append(row[2])
+
+
+    print(candidates)
+
+    # Compute Average Change (and round to 2 decimals)
+#    average_change = sum_of_monthly_change / (months - 1)
+#    average_change = float("{0:.2f}".format(average_change))
+
+    # Print Report
+
+    # Print to text file
+#    textfile = open("financial_analysis.txt", "w") 
+ 
+#    textfile.write("Financial Analysis\n") 
+ 
+#    textfile.close() 
+
+
